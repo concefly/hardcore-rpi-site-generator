479a080b06997d75fd23ba0d41265053739fa3dd
concefly <concefly@foxmail.com>
2020-02-09 22:27:03 +0800

    update: test

--- src/generator/post/__test__/index.test.ts
+++ src/generator/post/__test__/index.test.ts
@@ -5,9 +5,17 @@ import { Markdown } from '../../../page/Markdown';
 import { BaseTemplateRender } from '../../../template/BaseTemplateRender';
 
 test('PostGenerator:/_posts/a.md', async t => {
-  const collection = new Collection().set(
-    new Markdown(Buffer.from('a', 'utf-8'), '/_posts/a.md', '_posts/a.md', { mime: 'text/plain' })
-  );
+  const collection = new Collection()
+    .set(
+      new Markdown(Buffer.from('a', 'utf-8'), '/_posts/a.md', '_posts/a.md', { mime: 'text/plain' })
+    )
+    .set(new Markdown(Buffer.from('a', 'utf-8'), '/about.md', 'about.md', { mime: 'text/plain' }))
+    .set(
+      new Markdown(Buffer.from('a', 'utf-8'), '/xxx/about.md', 'xxx/about.md', {
+        mime: 'text/plain',
+      })
+    );
+
   const templateRender = new BaseTemplateRender('/', 'dev');
   const generator = new PostGenerator(collection, templateRender);
 
@@ -16,27 +24,45 @@ test('PostGenerator:/_posts/a.md', async t => {
   t.snapshot(result);
 });
 
-test('PostGenerator:/about', async t => {
-  const collection = new Collection().set(
-    new Markdown(Buffer.from('a', 'utf-8'), '/about.md', 'about.md', { mime: 'text/plain' })
-  );
+test('PostGenerator:创建时间排序', async t => {
+  const collection = new Collection()
+    .set(
+      new Markdown(
+        Buffer.from(
+          `
+---
+createDate: 1
+---
+创建 1`,
+          'utf-8'
+        ),
+        '/_posts/1.md',
+        '_posts/1.md',
+        { mime: 'text/plain' }
+      )
+    )
+    .set(
+      new Markdown(
+        Buffer.from(
+          `
+---
+createDate: 2
+---
+创建 2`,
+          'utf-8'
+        ),
+        '/_posts/2.md',
+        '_posts/2.md',
+        {
+          mime: 'text/plain',
+        }
+      )
+    );
+
   const templateRender = new BaseTemplateRender('/', 'dev');
   const generator = new PostGenerator(collection, templateRender);
 
   const result = await generator.generate();
 
-  t.snapshot(result);
-});
-
-
-test('PostGenerator:/xxx/about', async t => {
-  const collection = new Collection().set(
-    new Markdown(Buffer.from('a', 'utf-8'), '/xxx/about.md', 'xxx/about.md', { mime: 'text/plain' })
-  );
-  const templateRender = new BaseTemplateRender('/', 'dev');
-  const generator = new PostGenerator(collection, templateRender);
-
-  const result = await generator.generate();
-
-  t.snapshot(result);
+  t.deepEqual(result.globalInfo.post.list, ['/post/2.html', '/post/1.html']);
 });
