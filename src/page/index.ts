99393605fbd110413543e3907e386366ec079054
concefly <concefly@foxmail.com>
2020-02-16 21:35:24 +0800

    update: getFileType

--- src/page/index.ts
+++ src/page/index.ts
@@ -2,8 +2,24 @@ import * as fs from 'fs';
 import * as path from 'path';
 import { Markdown } from './Markdown';
 import { BasePage, BaseTextPage, BaseBinPage } from './BasePage';
-import { Magic, MAGIC_MIME_TYPE } from 'mmmagic';
 import { BaseTemplateRender } from '../template/BaseTemplateRender';
+import * as mimeTypes from 'mime-types';
+
+// mime 类型表：https://developer.mozilla.org/zh-CN/docs/Web/HTTP/Basics_of_HTTP/MIME_Types
+async function getFileType(
+  filePath?: string
+): Promise<{
+  mime: string;
+  ext: string;
+}> {
+  const ext = path.extname(filePath);
+  const mime = mimeTypes.lookup(filePath);
+
+  if (mime) return { mime, ext };
+
+  // 识别不到 mime 就认为是二进制
+  return { ext, mime: 'application/octet-stream' };
+}
 
 export async function createPage(
   filePath: string,
@@ -12,21 +28,13 @@ export async function createPage(
 ): Promise<BasePage> {
   if (!fs.existsSync(filePath)) return null;
 
-  const extname = path.extname(filePath);
   const raw = fs.readFileSync(filePath);
-
-  // mime 类型表：https://developer.mozilla.org/zh-CN/docs/Web/HTTP/Basics_of_HTTP/MIME_Types
-  const mime = await new Promise<string>((resolve, reject) =>
-    new Magic(filePath, MAGIC_MIME_TYPE).detect(raw, (err, res) => {
-      if (err) return reject(err);
-      resolve(res);
-    })
-  );
+  const { mime, ext } = await getFileType(filePath);
 
   // 文本类型
   if (mime.match(/^text/)) {
     // markdown
-    if (extname === '.md') {
+    if (ext === '.md') {
       return new Markdown(raw, filePath, relPath, { mime }).setTemplateRender(templateRender);
     }
 
